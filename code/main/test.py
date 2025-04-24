6ece0f6c698cfd4d569571c5e473d1306446660f
Vit Palaga <vit@palaga.org>
2025-04-24 13:15:16 +0200

    find offset

--- code/main/test.py
+++ code/main/test.py
@@ -26,13 +26,14 @@ file to test the code in different ways
 """
 
 def mam_ret(x, y):
-    Mov.move(-1 *(x + 5), -y, poss_check=False)
+    Mov.move(x + 5
+             , -y, poss_check=False)
 
 
 if __name__ == "__main__":
     try:
         #Mov.open()
-        Mov.move(-20, 0, poss_check=False)
+        #Mov.move(-20, 0, poss_check=False)
 
         mam_ret(0, 0)
 
@@ -40,7 +41,7 @@ if __name__ == "__main__":
 
         Mov.move(-steps, 0)
         time.sleep(3)
-        mam_ret(-steps, 0)
+        mam_ret(steps, 0)
 
         #Mov.ret()
 
